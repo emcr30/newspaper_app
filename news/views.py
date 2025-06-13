ad265484df5dab4c15ea4908eb916567527b3215
Paul Armas Rivera <parmasr@ulasalle.edu.pe>
2025-06-13 17:54:03 -0500

    Initial commit

---
+++ news/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def newsView(request):
+    return HttpResponse('Hello Web from view function. Evelyn Chipana 2025/06/13')
